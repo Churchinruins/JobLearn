ffc43485eca59b69ae6820ca4db8ab60a78d2610
Churchinruins <surhoisaev@gmail.com>
2024-10-02 14:30:06 +0300

    change:type

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-alert('Hello')
-console.log('Ohayo')
-let title = 10;
-let screens = 20;
-let screenPrice = 30;
+let title = 'JobLearn';
+console.log(typeof title);
+let screens = 'Простые, Сложные, Интерактивные';
+console.log(screens.length)
+console.log(screens.toLowerCase())
+console.log(screens.split())
+let screenPrice = 300;
+console.log(screenPrice);
 let rollback = 40;
-const fullPrice = 50;
-const adaptive = 60
+console.log(rollback);
+let fullPrice = 500000;
+console.log(typeof fullPrice);
+console.log(fullPrice * (rollback / 100));
+let adaptive = true;
+console.log(typeof adaptive);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
