3da00584ce1e15c01d201f302f29ae3c32890fc2
Churchinruins <surhoisaev@gmail.com>
2024-10-02 16:06:22 +0300

    fics:type

--- js/script.js
+++ js/script.js
@@ -2,16 +2,15 @@ let title = 'JobLearn';
 console.log(typeof title);
 let screens = 'Простые, Сложные, Интерактивные';
 console.log(screens.length)
-console.log(screens.toLowerCase())
-console.log(screens.split())
+console.log(screens.toLowerCase().split(', '))
 let screenPrice = 300;
 console.log(screenPrice);
 let rollback = 40;
 console.log(rollback);
 let fullPrice = 500000;
 console.log(typeof fullPrice);
-console.log(fullPrice * (rollback / 100));
 let adaptive = true;
 console.log(typeof adaptive);
+console.log(`Процент отката посреднику за работу ${fullPrice * (rollback / 100)}`);
 console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
 console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
