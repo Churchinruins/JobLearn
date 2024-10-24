d452975d7ad33e9ad559783c565e1c1e604c95d5
Churchinruins <surhoisaev@gmail.com>
2024-10-24 15:52:46 +0300

    fics:function

--- js/script.js
+++ js/script.js
@@ -9,9 +9,6 @@ let service1 = prompt('Какой дополнительный тип услуг
 let servicePrice1 = +prompt('Сколько это будет стоить?',);
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?',);
-const allServicePrices = getAllServicesPrices();
-const fullPrice = getFullPrice(screenPrice, allServicePrices);
-let servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
 
 
 const getRollbackMessage = function (price) {
@@ -25,26 +22,24 @@ const getRollbackMessage = function (price) {
         return "Что то пошло не так"
     }
 }
-function getAllServicesPrices() {
+const getAllServicesPrices = function () {
     return servicePrice1 + servicePrice2
 }
+const allServicePrices = getAllServicesPrices();
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
+let servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+
 function getFullPrice(screenPrice, allServicePrices) {
     return screenPrice + allServicePrices;
 }
-function getTitle() {
-    if (title.length === 0) return title;
-    return title.charAt(0).toUpperCase() + title.slice(1).toLowerCase();
+function getTitle(title) {
+    if (title.trim().length === 0) return title;
+    return title.trim().charAt(0).toUpperCase() + title.trim().slice(1).toLowerCase();
 }
 function getServicePercentPrices(fullPrice, rollback) {
     return fullPrice - (fullPrice * (rollback / 100))
 }
 
-
-console.log(getTitle());
 console.log(getRollbackMessage(fullPrice));
-console.log(typeof screenPrice);
-console.log(typeof adaptive);
 console.log(screens);
 console.log(servicePercentPrice);
-
-console.log(" Стоимость верстки экранов " + screenPrice + " рублей/юаней/долларов ")
